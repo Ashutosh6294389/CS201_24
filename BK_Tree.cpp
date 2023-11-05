a2685bbc4b298392ebe85a1a47ed16d4e9970aa3
Suryadeep2004 <146474801+Suryadeep2004@users.noreply.github.com>
2023-11-05 23:34:03 +0530

    Update BK_Tree.cpp

--- BK_Tree.cpp
+++ BK_Tree.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
-#include"BK_Tree.hpp"
+#include "BK_Tree.hpp" // Include the necessary header file
 using namespace std;
 
-BkNode rootNode;
-BkNode BK_Tree[BMAX];
-int ptr;
+BkNode rootNode; // Declare the root node of the BK-Tree
+BkNode BK_Tree[BMAX]; // Declare an array to store BK-Tree nodes
+int ptr; // Declare a pointer to keep track of the current node
 
+// Function to create a new BK-Tree node with a given string
 BkNode createNode(string s)
 {
     BkNode n;
@@ -14,16 +15,20 @@ BkNode createNode(string s)
         n.next[i] = 0;
     return n;
 }
+
+// Function to calculate the edit distance between two strings s1 and s2
 int editDistance(string s1, string s2)
 {
     int n = s1.length(), m = s2.length();
     int dp[n + 1][m + 1];
 
+    // Initialize the dynamic programming matrix
     for (int i = 0; i <= n; i++)
         dp[i][0] = i;
     for (int i = 0; i <= m; i++)
         dp[0][i] = i;
 
+    // Compute the edit distance using dynamic programming
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= m; j++)
@@ -39,15 +44,17 @@ int editDistance(string s1, string s2)
     }
     return dp[n][m];
 }
+
+// Function to add a node to the BK-Tree
 void addNode(BkNode &rootNode, BkNode &currentNode)
 {
     if (rootNode.word == "")
     {
-        rootNode = currentNode;
+        rootNode = currentNode; // If the root node is empty, set it to the current node
         return;
     }
 
-    int d = editDistance(rootNode.word, currentNode.word);
+    int d = editDistance(rootNode.word, currentNode.word); // Calculate the edit distance
     if (BK_Tree[rootNode.next[d]].word == "")
     {
         ptr++;
@@ -55,28 +62,32 @@ void addNode(BkNode &rootNode, BkNode &currentNode)
         rootNode.next[d] = ptr;
     }
     else
-        addNode(BK_Tree[rootNode.next[d]], currentNode);
+        addNode(BK_Tree[rootNode.next[d]], currentNode); // Recursively add the node
 }
+
+// Function to get similar words to a given string in the BK-Tree
 vector<string> getSimilarWords(BkNode &rootNode, string s)
 {
     vector<string> similarWords;
     if (rootNode.word == "")
         return similarWords;
 
-    int d = editDistance(rootNode.word, s);
+    int d = editDistance(rootNode.word, s); // Calculate the edit distance
     if (d <= TOL)
         similarWords.push_back(rootNode.word);
 
     int x = max(0, d - TOL);
     while (x <= d + TOL)
     {
-        vector<string> temp = getSimilarWords(BK_Tree[rootNode.next[x]], s);
+        vector<string> temp = getSimilarWords(BK_Tree[rootNode.next[x]], s); // Recursively search for similar words
         for (int i = 0; i < temp.size(); i++)
             similarWords.push_back(temp[i]);
         x++;
     }
     return similarWords;
 }
+
+// Function to return a pointer to the root node of the BK-Tree
 BkNode* ReturnBNode(){
     return &rootNode;
 }
