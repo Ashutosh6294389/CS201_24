7fd8e0dd4c250031270400595bbc13938821c5c2
Suryadeep2004 <146474801+Suryadeep2004@users.noreply.github.com>
2023-11-05 23:36:29 +0530

    Update PatternSearch.c

--- PatternSearch.c
+++ PatternSearch.c
@@ -1,86 +1,96 @@
-#include"Suffix_tree.h"
-#include"PatternSearch.h"
-#include<stdio.h>
-int* test2;
-int ind = 0;
-int traverseEdge(char *str, int idx, int start, int end,char* text)
+#include "Suffix_tree.h"
+#include "PatternSearch.h"
+#include <stdio.h>
+
+int* test2; // Pointer to store results
+int ind = 0; // Index for tracking results
+
+// Function to traverse an edge in the suffix tree and compare it with a given string
+int traverseEdge(char *str, int idx, int start, int end, char* text)
 {
     int k = 0;
-    for(k=start; k<=end && str[idx] != '\0'; k++, idx++)
+    for (k = start; k <= end && str[idx] != '\0'; k++, idx++)
     {
-        if(text[k] != str[idx])
-            return -1; 
+        if (text[k] != str[idx])
+            return -1; // Mismatch, return -1
     }
-    if(str[idx] == '\0')
-        return 1;
-    return 0;  
+    if (str[idx] == '\0')
+        return 1; // Exact match, return 1
+    return 0; // Partial match, return 0
 }
+
+// Function to perform a recursive traversal to count leaf nodes in the suffix tree
 int doTraversalToCountLeaf(SNode *n)
 {
-    if(n == NULL)
+    if (n == NULL)
         return 0;
-    if(n->suffixIndex > -1)
+    if (n->suffixIndex > -1)
     {
-        test2[ind++] = n->suffixIndex;
+        test2[ind++] = n->suffixIndex; // Store the suffix index in the results array
         return 1;
     }
     int count = 0;
     int i = 0;
     for (i = 0; i < MAX; i++)
     {
-        if(n->children[i] != NULL)
+        if (n->children[i] != NULL)
         {
             count += doTraversalToCountLeaf(n->children[i]);
         }
     }
     return count;
 }
-  
+
+// Function to count the number of leaf nodes in the suffix tree
 int countLeaf(SNode *n)
 {
-    if(n == NULL)
+    if (n == NULL)
         return 0;
     return doTraversalToCountLeaf(n);
 }
-  
-int doTraversal(SNode *n, char* str, int idx,char* text)
+
+// Function to perform a recursive traversal in the suffix tree to search for a string
+int doTraversal(SNode *n, char* str, int idx, char* text)
 {
-    if(n == NULL)
+    if (n == NULL)
     {
-        return -1;
+        return -1; // No match, return -1
     }
     int res = -1;
-    if(n->start != -1)
+    if (n->start != -1)
     {
-        res = traverseEdge(str, idx, n->start, *(n->end),text);
-        if(res == -1)
-            return -1;
-        if(res == 1) 
+        res = traverseEdge(str, idx, n->start, *(n->end), text); // Compare the edge with the string
+        if (res == -1)
+            return -1; // Mismatch, return -1
+        if (res == 1) 
         {
-            if(n->suffixIndex > -1){
-                printf("\nOrder count: 1");
+            if (n->suffixIndex > -1)
+            {
+                printf("\nOrder count: 1"); // Suffix found, print order count
                 test2[ind++] = n->suffixIndex;
             }
             else
-                printf("\nOrder count: %d", countLeaf(n));
+                printf("\nOrder count: %d", countLeaf(n)); // Count and print the number of leaf nodes
             return 1;
         }
     }
     idx = idx + edgeLength(n);
-    if(n->children[str[idx]] != NULL)
-        return doTraversal(n->children[str[idx]], str, idx,text);
+    if (n->children[str[idx]] != NULL)
+        return doTraversal(n->children[str[idx]], str, idx, text);
     else
-        return -1;  // no match
+        return -1; // No match
 }
-void checkForSubString(char* str,SNode* root,char* text,int* pos)
+
+// Function to check for a substring in the suffix tree and store the results
+void checkForSubString(char* str, SNode* root, char* text, int* pos)
 {
-    test2 = pos;
-    int res = doTraversal(root, str, 0,text);
-    test2[ind] = -1;
-    if(res == 1){
-        ind = 0;
+    test2 = pos; // Set the results array
+    int res = doTraversal(root, str, 0, text); // Search for the substring
+    test2[ind] = -1; // Mark the end of the results
+    if (res == 1){
+        ind = 0; // Reset the results index
     }
     else{
-        printf("\n%s is NOT ordered yet\n", str);
+        printf("\n%s is NOT ordered yet\n", str); // String not found in the tree
     }
 }
